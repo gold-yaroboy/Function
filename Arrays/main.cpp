884f61645ae77a39476ad67590129d6c20ca62b8
ilyav <ilyav@ILIA>
2025-05-19 01:02:49 +0500

    char error

--- Arrays/main.cpp
+++ Arrays/main.cpp
@@ -6,36 +6,48 @@ using std::endl;
 //lesson
 void FillRand(int arr[], const int n, int minRand = 0, int maxRand = 100);
 void FillRand(double arr[], const int n, int minRand = 0, int maxRand = 100);
+void FillRand(float arr[], const int n, int minRand = 0, int maxRand = 100);
+void FillRand(char arr[], const int n, int minRand = 0, int maxRand = 100);
 
 void Print(int arr[], const int n);
 void Print(double arr[], const int n);
+void Print(float arr[], const int n);
+void Print(char arr[], const int n);
 
 void Sort(int arr[], const int n);
 void Sort(double arr[], const int n);
+void Sort(float arr[], const int n);
+void Sort(char arr[], const int n);
 //home
 void Sum(int arr[], const int n);
 void Sum(double arr[], const int n);
 void Sum(float arr[], const int n);
+void Sum(char arr[], const int n);
 
 void Avg(int arr[], const int n);
 void Avg(double arr[], const int n);
 void Avg(float arr[], const int n);
+void Avg(char arr[], const int n);
 
 void MinValueIn(int arr[], const int arr_size);
 void MinValueIn(double arr[], const int arr_size);
 void MinValueIn(float arr[], const int arr_size);
+void MinValueIn(char arr[], const int arr_size);
 
 void MaxValueIn(int arr[], const int arr_size);
 void MaxValueIn(double arr[], const int arr_size);
 void MaxValueIn(float arr[], const int arr_size);
+void MaxValueIn(char arr[], const int arr_size);
 
 void ShiftLeft(int arr[], const int arr_size);
 void ShiftLeft(double arr[], const int arr_size);
 void ShiftLeft(float arr[], const int arr_size);
+void ShiftLeft(char arr[], const int arr_size);
 
 void ShiftRight(int arr[], const int arr_size);
 void ShiftRight(double arr[], const int arr_size);
 void ShiftRight(float arr[], const int arr_size);
+void ShiftRight(char arr[], const int arr_size);
 
 void main()
 {
@@ -81,6 +93,19 @@ void main()
 	MaxValueIn(crr, size);
 	ShiftLeft(crr, size);
 	ShiftRight(crr, size);
+	cout << "--------------------" << endl;
+	const int Size = 10;
+	char drr[size];
+	FillRand(drr, Size);
+	Print(drr, Size);
+	Sort(drr, Size);
+	Print(drr, Size);
+	Sum(drr, Size);
+	Avg(drr, Size);
+	MinValueIn(drr, Size);
+	MaxValueIn(drr, Size);
+	ShiftLeft(drr, Size);
+	ShiftRight(drr, Size);
 }
 //lesson
 void FillRand(int arr[], const int n, int minRand, int maxRand)
@@ -110,6 +135,16 @@ void FillRand(float arr[], const int n, int minRand, int maxRand)
 		arr[i] /= 100;
 	}
 }
+void FillRand(char arr[], const int n, int minRand, int maxRand)
+{
+	char(minRand *= 100);
+	char(maxRand *= 100);
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+		arr[i] /= 100;
+	}
+}
 
 void Print(int arr[], const int n)
 {
@@ -128,6 +163,14 @@ void Print(double arr[], const int n)
 	cout << endl;
 }
 void Print(float arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
+void Print(char arr[], const int n)
 {
 	for (int i = 0; i < n; i++)
 	{
@@ -181,6 +224,21 @@ void Sort(float arr[], const int n)
 		}
 	}
 }
+void Sort(char arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				float buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
 
 //home
 void Sum(int arr[], const int arr_size)
@@ -198,6 +256,12 @@ void Sum(double arr[], const int arr_size)
 void Sum(float arr[], const int arr_size)
 {
 	float Sum = 0;
+	for (int i = 0; i < arr_size; i++)Sum += arr[i];
+	cout << Sum << endl;
+}
+void Sum(char arr[], const int arr_size)
+{
+	char Sum = 0;
 	for (int i = 0; i < arr_size; i++)Sum += arr[i];
 	cout << Sum << endl;
 }
@@ -219,6 +283,13 @@ void Avg(float arr[], const int arr_size)
 	float Sum = 0;
 	for (int i = 0; i < arr_size; i++)Sum += arr[i];
 	cout << (double)Sum/arr_size << endl;
+}
+void Avg(char arr[], const int arr_size)
+{
+	char Sum = 0;
+	for (int i = 0; i < arr_size; i++)Sum += arr[i];
+	char Avg = (double)Sum / arr_size;
+	cout << Avg << endl;
 }
 
 void MinValueIn(int arr[], const int arr_size)
@@ -248,6 +319,15 @@ void MinValueIn(float arr[], const int arr_size)
 	}
 	cout <<  min << endl;
 }
+void MinValueIn(char arr[], const int arr_size)
+{
+	char min = arr[0];
+	for (int i = 0; i < arr_size; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+	}
+	cout <<  min << endl;
+}
 
 void MaxValueIn(int arr[], const int arr_size)
 {
@@ -270,6 +350,15 @@ void MaxValueIn(double arr[], const int arr_size)
 void MaxValueIn(float arr[], const int arr_size)
 {
 	float max = arr[0];
+	for (int i = 0; i < arr_size; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+	}
+	cout <<  max << endl;
+}
+void MaxValueIn(char arr[], const int arr_size)
+{
+	char max = arr[0];
 	for (int i = 0; i < arr_size; i++)
 	{
 		if (arr[i] > max)max = arr[i];
@@ -316,6 +405,19 @@ void ShiftLeft(float arr[], const int arr_size)
 	}
 	cout << endl;
 }
+void ShiftLeft(char arr[], const int arr_size)
+{
+	char temp;
+	for (int i = 0; i < arr_size; i++)
+	{
+		temp = arr[arr_size - 1];
+		for (int i = arr_size - 1; i > 0; i--)arr[i] = arr[i - 1];
+		arr[0] = temp;
+		for (int i = 0; i < arr_size; i++)cout << arr[i] << "\t";
+		cout << endl;
+	}
+	cout << endl;
+}
 
 void ShiftRight(int arr[], const int arr_size)
 {
@@ -346,6 +448,18 @@ void ShiftRight(float arr[], const int arr_size)
 	for (int j = 0; j < arr_size; j++)
 	{
 		float buf;
+		buf = arr[0];
+		for (int i = 1; i < arr_size; i++) arr[i - 1] = arr[i];
+		arr[arr_size - 1] = buf;
+		for (int i = 0; i < arr_size; i++)cout << arr[i] << "\t";
+		cout << endl;
+	}
+}
+void ShiftRight(char arr[], const int arr_size)
+{
+	for (int j = 0; j < arr_size; j++)
+	{
+		char buf;
 		buf = arr[0];
 		for (int i = 1; i < arr_size; i++) arr[i - 1] = arr[i];
 		arr[arr_size - 1] = buf;
